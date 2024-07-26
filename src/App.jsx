e723a5b87a79deb75eaf1be9be56fc2676a4679b
tanmay-tiwari-20 <tanmaytiwari200905@gmail.com>
2024-07-26 14:46:48 +0530

    Landing page don

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Work from './Components/Work'
+import Stripes from './Components/Stripes'
 
 function App() {
   return (
-    <div className='w-full h-screen bg-zinc-900 text-white'>
+    <div className='w-full h-screen bg-black text-white'>
       <Navbar />
+      <Work />
+      <Stripes />
     </div>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,34 @@
+import { space } from "postcss/lib/list";
 import React from "react";
+import Button from "./Button";
 
 function Navbar() {
   return (
-    <div className="max-w-screen-xl py-6 mx-auto flex items-center">
+    <div className="max-w-screen-xl py-6 mx-auto flex items-center justify-between border-b-[1px] border-zinc-700">
+      <div className="nleft flex items-center">
       <img
         src="https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg"
         alt=""
       />
       <div className="links flex gap-14 ml-20">
-        {["Home", "Work", "Culture"].map((elem, index) => (
-          <a className="text-sm flex items-center gap-1" href="#">
-            {index===1 && (<span style={{boxShadow: "0 0 0.45rem #00FF19"}} className="inline-block w-1 h-1 rounded-full bg-green-500"></span>)}
-            {elem}
-          </a>
-        ))}
+        {["Home", "Work", "Culture", "", "News"].map((elem, index) =>
+          elem.length === 0 ? (
+            <span className="w-[2px] h-7 bg-zinc-700"></span>
+          ) : (
+            <a className="text-sm flex items-center gap-1" href="#">
+              {index === 1 && (
+                <span
+                  style={{ boxShadow: "0 0 0.45rem #00FF19" }}
+                  className="inline-block w-[6px] h-[6px] rounded-full bg-green-500"
+                ></span>
+              )}
+              {elem}
+            </a>
+          )
+        )}
       </div>
+      </div>
+      <Button />
     </div>
   );
 }
